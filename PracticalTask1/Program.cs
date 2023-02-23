3afced5d83348a034f6a8242759ee94af5910bcc
Kubra <cskubra@gmail.com>
2023-02-23 22:54:45 +0300

    Proje dosyası ekle.

---
+++ PracticalTask1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class ArrayExample
+{
+    static void Main()
+    {
+        char[] letters = { 'k', 'u', 'b', 'r', 'a', ' ', 'c', 'a', 'l', 'i', 's', 'k','a', 'n' };
+        string name = "";
+        int[] a = new int[14];
+        for (int i = 0; i < letters.Length; i++)
+        {
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
+        }
+        Console.ReadKey();
+    }
+
+    static void SendMessage(string name, int msg)
+    {
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
+    }
+}
